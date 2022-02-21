6295e14aae2857022209f960471860cdc4e675c9
Zastenchivyy-Smolensky <s20c2007@bene.fit.ac.jp>
2022-02-21 23:59:24 +0900

    .

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :validatable
+  
+    # フォローする側から中間テーブルへのアソシエーション
+    has_many :relationships, foreign_key: :following_id
+    # フォローする側からフォローされたユーザを取得する
+    has_many :followings, through: :relationships, source: :follower
+  
+    # フォローされる側から中間テーブルへのアソシエーション
+    has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :follower_id
+    # フォローされる側からフォローしているユーザを取得する
+    has_many :followers, through: :reverse_of_relationships, source: :following
+  
+    # あるユーザが引数で渡されたuserにフォローされているか調べるメソッド
+    def is_followed_by?(user)
+      reverse_of_relationships.find_by(following_id: user.id).present?
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_12_163056) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_13_010144) do
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follwing_id", null: false
+    t.integer "follower_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
